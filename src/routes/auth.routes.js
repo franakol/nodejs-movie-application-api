ec0594ec93cbd723f6a7cdcc36b8d94fa746976a
aline <alineoniwemwari@gmailcom>
2025-04-25 08:56:16 +0200

    Addition of mongoose

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose=require('mongoose');
 const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
